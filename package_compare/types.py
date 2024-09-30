6bb252de71a719e2db2675db30a21de712b3e399
mitisit <29955395+mitisit89@users.noreply.github.com>
2024-09-30 19:28:47 +0300

    chore:add types

--- package_compare/types.py
+++ package_compare/types.py
@@ -16,3 +16,10 @@ class RequestData(TypedDict):
     request_args: dict[str, str] | dict[None, None]
     length: int
     packages: list[PackageInfo]
+
+
+class Comparison(TypedDict):
+    only_in_p10: list[str]
+    only_in_sisyphus: list[str]
+    p10_newer: list[str]
+    sisyphus_newer: list[str]
